8a0754679e6ad347a4cd75e22595186b3dca2360
shivansh verma <sshivansh2712@gmail.com>
2025-04-10 12:30:16 +0530

    added user loaction and homepage

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Text } from 'react-native';
-import { useFonts } from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
-import { useCallback } from 'react';
-import LoginScreen from './App/Screen/LoginScreen';
-import { ClerkProvider, SignedIn, SignedOut } from '@clerk/clerk-expo';
-import { NavigationContainer } from '@react-navigation/native';
-import TabNavigation from './App/Screen/Navigations/TabNavigation';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, Text } from "react-native";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback, useEffect, useState } from "react";
+import LoginScreen from "./App/Screen/LoginScreen";
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
+import { NavigationContainer } from "@react-navigation/native";
+import TabNavigation from "./App/Screen/Navigations/TabNavigation";
+import * as Location from "expo-location";
+import { UserLocationContext } from "./App/Context/UserLocationContext";
 
 SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  const [fontsLoaded, fontError] = useFonts({
-    'Inter-Black': require('./assets/fonts/Outfit-Black.ttf'),
-    'Outfit-Medium': require('./assets/fonts/Outfit-Medium.ttf'),
-  });
-  
+  const [fontsLoaded, fontError] =
+    useFonts({
+      "Inter-Black": require("./assets/fonts/Outfit-Black.ttf"),
+      "Outfit-Medium": require("./assets/fonts/Outfit-Medium.ttf"),
+    }) || [];
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    async function getCurrentLocation() {
+      const { status } = await Location.requestForegroundPermissionsAsync(); // ✅ object destructuring
+
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      const location = await Location.getCurrentPositionAsync({});
+      setLocation(location.coords); // ✅ set location state
+    }
+
+    getCurrentLocation();
+  }, []);
+
+  let text = "Waiting...";
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
   const onLayoutRootView = useCallback(async () => {
     if (fontsLoaded || fontError) {
       await SplashScreen.hideAsync();
@@ -27,18 +55,22 @@ export default function App() {
   }
 
   return (
-    <ClerkProvider publishableKey={'pk_test_c3VwcmVtZS1zZWFsLTk0LmNsZXJrLmFjY291bnRzLmRldiQ'}>
-      <View style={styles.container} onLayout={onLayoutRootView}>
-        <SignedIn>
-          <NavigationContainer>
-            <TabNavigation />
-          </NavigationContainer>
-        </SignedIn>
-        <SignedOut>
-          <LoginScreen />
-        </SignedOut>
-        <StatusBar style="auto" />
-      </View>
+    <ClerkProvider
+      publishableKey={"pk_test_c3VwcmVtZS1zZWFsLTk0LmNsZXJrLmFjY291bnRzLmRldiQ"}
+    >
+      <UserLocationContext.Provider value={{ location, setLocation }}>
+        <View style={styles.container} onLayout={onLayoutRootView}>
+          <SignedIn>
+            <NavigationContainer>
+              <TabNavigation />
+            </NavigationContainer>
+          </SignedIn>
+          <SignedOut>
+            <LoginScreen />
+          </SignedOut>
+          <StatusBar style="auto" />
+        </View>
+      </UserLocationContext.Provider>
     </ClerkProvider>
   );
 }
@@ -46,7 +78,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     paddingTop: 25,
   },
 });
--- App/Screen/HomeScreen/AppMapView.jsx
+++ App/Screen/HomeScreen/AppMapView.jsx
@@ -1,23 +1,47 @@
- import { View, Text, StyleSheet,  } from 'react-native'
- import React from 'react'
- import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
- import MapViewStyle from './../Utils/MapViewStyle.json'
- export default function AppMapView() {
-   return (
-     <View>
-       <MapView style= {styles.map}
-       provider={PROVIDER_GOOGLE}
-       showsUserLocation={true}
-       cunstonMapStyle ={MapViewStyle} />
-     </View>
-   )
- }
- const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-    },
-    map: {
-      width: '100%',
-      height: '100%',
-    },
-  });
+import { View, Text, StyleSheet, Image } from "react-native";
+import React, { useContext } from "react";
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
+import MapViewStyle from "../Utils/MapViewStyle.json";
+import { UserLocationContext } from "../../Context/UserLocationContext";
+import carImg from "../../../assets/images/car-logo.png";
+
+export default function AppMapView() {
+  const { location, setLocation } = useContext(UserLocationContext);
+
+  return (
+    location?.latitude && (
+      <View>
+        <MapView
+          style={styles.map}
+          provider={PROVIDER_GOOGLE}
+          showsUserLocation={true}
+          customMapStyle={MapViewStyle}
+          region={{
+            latitude: location?.latitude,
+            longitude: location?.longitude,
+            latitudeDelta: 0.0422,
+            longitudeDelta: 0.0421,
+          }}
+        >
+          <Marker
+            coordinate={{
+              longitude: location?.longitude,
+              latitude: location?.latitude,
+            }}
+          >
+            <Image source={carImg} style={{ width: 60, height: 80 }} />
+          </Marker>
+        </MapView>
+      </View>
+    )
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+});
